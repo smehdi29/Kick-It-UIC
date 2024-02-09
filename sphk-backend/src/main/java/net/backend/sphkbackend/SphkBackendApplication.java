fc4376ecf920ab1de56517da51108320a853e72c
smehdi29 <syed.business29@gmail.com>
2024-02-08 21:45:45 -0600

    Initial commit

---
+++ sphk-backend/src/main/java/net/backend/sphkbackend/SphkBackendApplication.java
@@ -0,0 +1,13 @@
+package net.backend.sphkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SphkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SphkBackendApplication.class, args);
+	}
+
+}
---
+++ sphk-backend/src/test/java/net/backend/sphkbackend/SphkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.backend.sphkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SphkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
